a1aa608daab321bd6a4059f013004e438d440fc2
FlyaginAs <rus.reg.36@gmail.com>
2021-06-18 10:58:56 +0300

    rewrite serchlPanel to hooks

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,29 +1,59 @@
-import React, { Component } from 'react';
+import React, {  useState } from 'react';
 
 import './search-panel.css';
 
-export default class SearchPanel extends Component {
+const SearchPanel=(props)=> {
+  const [state,setState]=useState({term:'',});
+  // state = {
+  //   term: ''
+  // };
+  
 
-  state = {
-    term: ''
-  };
-
-  onTermChange = (e) => {
-    const {onSearchChange = () => {}} = this.props;
-    this.setState({
+  const onTermChange = (e) => {
+    const {onSearchChange = () => {}} = props;
+    setState({
       term: e.target.value
     });
 
     onSearchChange(e.target.value);
   };
 
-  render() {
+ 
     return (
+      
       <input type="text"
              className="form-control search-input"
              placeholder="type to search"
-             value={this.state.term}
-             onChange={ this.onTermChange } />
+             value={state.term}
+             onChange={ onTermChange } />
     );
-  };
-}
+  
+};
+
+export default SearchPanel;
+
+// export default class SearchPanel extends Component {
+
+//   state = {
+//     term: ''
+//   };
+
+//   onTermChange = (e) => {
+//     const {onSearchChange = () => {}} = this.props;
+//     this.setState({
+//       term: e.target.value
+//     });
+
+//     onSearchChange(e.target.value);
+//   };
+
+//   render() {
+//     return (
+//       <input type="text"
+//              className="form-control search-input"
+//              placeholder="type to search"
+//              value={this.state.term}
+//              onChange={ this.onTermChange } />
+//     );
+//   };
+// }
